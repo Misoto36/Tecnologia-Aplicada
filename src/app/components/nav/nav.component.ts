43f655c21511c65c43804713b921c80f0a36d3eb
Miguelangel <misoto36@hotmail.com>
2024-05-20 21:08:29 -0500

    Configuracion menu responsivo

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -6,8 +6,21 @@ import { RouterLink } from '@angular/router';
   standalone: true,
   imports: [RouterLink],
   templateUrl: './nav.component.html',
-  styleUrl: './nav.component.css'
+  styleUrl: './nav.component.css',
 })
 export class NavComponent {
+  menuIsOpen: boolean = false;
+  constructor() {
+    this.showMenu();
+  }
 
+  @ViewChild('btnMenu') button: ElementRef | undefined;
+  @ViewChild('menu') menu: ElementRef | undefined;
+
+  showMenu() {
+    this.button?.nativeElement.addEventListener('click', () => {
+      this.menu?.nativeElement.classList.toggle('hidden');
+      this.menuIsOpen =!this.menuIsOpen;
+    });
+  }
 }
